f59992142558ce27cf45058a1805bd85e9870bfd
Giuseppe Siragusa <giuseppe.siragusa@kaleyra.com>
2022-07-07 18:12:00 +0200

    feat: add test for Golang sorting method

--- services/sort_service_test.go
+++ services/sort_service_test.go
@@ -14,3 +14,11 @@ func TestSort(t *testing.T) {
 
 	require.Equal(t, []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, elements)
 }
+
+func TestGolangSort(t *testing.T) {
+	elements := []int{5, 1, 6, 2, 3, 9, 8, 0, 7, 4}
+
+	GolangSort(elements)
+
+	require.Equal(t, []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, elements)
+}
